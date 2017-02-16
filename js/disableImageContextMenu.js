aca0022b38bc8c880ea5340863fea94b3169ebd3
lieupieu <lieupieu@gmail.com>
2017-02-16 19:19:22 +0000

    Publishing to gh-pages

---
+++ js/disableImageContextMenu.js
@@ -0,0 +1,10 @@
+/*function context_menu(e) {
+    if (!e) var e = window.event;
+    var eTarget = (window.event) ? e.srcElement : e.target;
+ 
+    if (eTarget.nodeName == "IMG") {
+        return false;
+    }
+}
+
+document.oncontextmenu = context_menu;*/
