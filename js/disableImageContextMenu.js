a20747d6ad5b532566998e7cacc920b3f8093410
lieupieu <lieupieu@gmail.com>
2017-02-22 17:55:19 +0000

    Minified js, removed monokai css,code clean up

--- js/disableImageContextMenu.js
+++ js/disableImageContextMenu.js
@@ -1,10 +1,1 @@
-function context_menu(e) {
-    if (!e) var e = window.event;
-    var eTarget = (window.event) ? e.srcElement : e.target;
- 
-    if (eTarget.nodeName == "IMG") {
-        return false;
-    }
-}
-
-document.oncontextmenu = context_menu;
+function context_menu(e){if(!e){var e=window.event}var eTarget=(window.event)?e.srcElement:e.target;if(eTarget.nodeName=="IMG"){return false}}document.oncontextmenu=context_menu;
